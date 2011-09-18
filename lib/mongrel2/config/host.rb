a50c7cb3366db809615c6cf8dd6918afa64f27b0
Michael Granger <ged@FaerieMUD.org>
2011-09-18 12:51:02 -0700

    Config DSL: directory: Default the index file to index.html

--- lib/mongrel2/config/host.rb
+++ lib/mongrel2/config/host.rb
@@ -38,7 +38,7 @@ class Mongrel2::Config::Host < Mongrel2::Config( :host )
 
 		### Create a new Mongrel2::Config::Directory object for the specified +base+ and
 		### return it.
-		def directory( base, index_file, default_ctype='text/plain', opts={} )
+		def directory( base, index_file='index.html', default_ctype='text/plain', opts={} )
 			opts.merge!( :base => base, :index_file => index_file, :default_ctype => default_ctype )
 			return Mongrel2::Config::Directory.create( opts )
 		end
