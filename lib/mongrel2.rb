f20eee4e1dabcac400fb6af297b45c31ac2d4213
Michael Granger <ged@FaerieMUD.org>
2012-01-25 09:18:57 -0800

    Bumped minor version, updated history.

--- lib/mongrel2.rb
+++ lib/mongrel2.rb
@@ -14,7 +14,7 @@ module Mongrel2
 	abort "\n\n>>> Mongrel2 requires Ruby 1.9.2 or later. <<<\n\n" if RUBY_VERSION < '1.9.2'
 
 	# Library version constant
-	VERSION = '0.9.3'
+	VERSION = '0.10.0'
 
 	# Version-control revision constant
 	REVISION = %q$Revision$
