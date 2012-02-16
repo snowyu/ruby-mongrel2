b474baf699e59cb2d4d19b246f981cbf0d55484a
Michael Granger <ged@FaerieMUD.org>
2012-02-15 19:54:47 -0800

    Flesh out docs for the Filter config class.

--- lib/mongrel2/config/filter.rb
+++ lib/mongrel2/config/filter.rb
@@ -5,7 +5,29 @@ require 'tnetstring'
 require 'mongrel2' unless defined?( Mongrel2 )
 require 'mongrel2/config' unless defined?( Mongrel2::Config )
 
-# Mongrel2 Host configuration class
+# Mongrel2 Filter configuration class
+#
+#   # Using the config DSL:
+#   filter '/usr/local/lib/mongrel2/filters/null.so',
+#       extensions: ["*.html", "*.txt"],
+#       min_size: 1000
+#
+#   # Which is the same as:
+#   Mongrel2::Config::Filter.create(
+#       mame: '/usr/local/lib/mongrel2/filters/null.so',
+#       settings: {
+#         extensions: ["*.html", "*.txt"],
+#         min_size: 1000
+#       }
+#
+#   # Or:
+#   server.add_filter(
+#       mame: '/usr/local/lib/mongrel2/filters/null.so',
+#       settings: {
+#         extensions: ["*.html", "*.txt"],
+#         min_size: 1000
+#       })
+#
 class Mongrel2::Config::Filter < Mongrel2::Config( :filter )
 
 	### As of Mongrel2/1.7.5:
